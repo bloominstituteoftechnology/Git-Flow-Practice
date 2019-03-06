c894ca0d7a5359ef52b8aa2fd3483c22dbf1048c
Myke47 <mychel.larson@gmail.com>
2019-03-06 09:23:01 -0700

    added new comment

--- newFile.js
+++ newFile.js
@@ -1,1 +1,2 @@
-// Oh hello there, I've made a change to a new file, added and commited all through git!
+// Oh hello there, I've made a change to a new file, added and commited all through git! 
+// New comment being added here!
