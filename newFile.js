a9158d059505c67c7513e491ddebfbdf45fb26c2
Eddie Madrigal <edmadrigal@yahoo.com>
2020-01-13 17:09:36 -0800

    File doubles keyboard number input

--- newFile.js
+++ newFile.js
@@ -1,5 +1,12 @@
-function doubleNum(a) {     
-    return 2 * a;
-}
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-console.log(doubleNum(99));
+readline.question(`Enter a number to double: `, (myNum) => {
+    const doubleNum = (num) => 2 * num;
+    const displayNewNum = (n, callback) => callback(doubleNum(n));
+    const result = displayNewNum(myNum, value => `Your ${myNum} was doubled to ${value}`);
+    console.log(result);
+    readline.close()
+})
